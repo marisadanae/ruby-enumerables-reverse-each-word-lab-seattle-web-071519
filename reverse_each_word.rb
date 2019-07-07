4bfa673e26d964d5e7b2601de7c8707ab0eb652b
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-07 21:22:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
 
 def reverse_each_word(string)
  reversed_string = ''
- 
- i = 0 
- while i < string.length 
+
+ i = 0
+ while i < string.length
   reversed_string = string[i] + reversed_string
-  i += 1 
+  i += 1
  end
- 
+
  return reversed_string
 end
