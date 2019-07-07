aa2c22c9b228267fd1960960bbd41897ffb3913c
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-07 21:22:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
 
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
-    word.reverse
-  end
-  reversed.join("")
+def reverse_each_word(string)
+ reversed_string = ''
+ 
+ i = 0 
+ while i < string.length 
+  reversed_string = string[i] + reversed_string
+  i += 1 
+ end
+ 
+ return reversed_string
 end
