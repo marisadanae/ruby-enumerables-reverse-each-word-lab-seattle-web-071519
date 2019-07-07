72b70ca9f9387aee6b138ed5281d562445b7bb68
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-07-07 21:15:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 
-def reverse_each_word(){
-  array = ["Hello there, and how are you?"]
-  array.map{|item| "#{item.reverse_each_word}"}
-end
-return()
+def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+    word.reverse
+  end
+  reversed.join("")
+end 
+  
+
